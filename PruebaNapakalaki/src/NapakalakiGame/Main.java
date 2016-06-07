c40b69c4573911bc91f3c9d60b9fd6b8eaab394e
Montse Rodríguez Zamorano <montse@192.168.1.111>
2016-06-07 23:43:05 +0200

    tester

---
+++ PruebaNapakalaki/src/NapakalakiGame/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NapakalakiGame;
+import GUI.NapakalakiView;
+import GUI.Dice;
+import GUI.PlayerNamesCapture;
+import java.util.ArrayList;
+/**
+ *
+ * @author Minim
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <String> names = new ArrayList();    
+        Napakalaki game = Napakalaki.getInstance();
+        NapakalakiView napakalakiView = new NapakalakiView();
+        PlayerNamesCapture namesCapture=new PlayerNamesCapture(napakalakiView,true);
+        
+        names = namesCapture.getNames();
+        Dice.createInstance(napakalakiView);
+        game.initGame(names);
+        napakalakiView.setNapakalaki(game);
+        //napakalakiView.showView();
+    }
+}
